68d3abbdc3a0bcb9bfacbdb7cf061c86b0459324
skc-garimella <skc.garimella@gmail.com>
2016-12-11 22:42:56 -0600

    commit

--- config/router.js
+++ config/router.js
@@ -5,9 +5,7 @@ const battle_controller = require('../controllers/battles');
 
 
 module.exports = function(app) {
-  app.get('/index', battle_controller.index);
-  app.get('/api/', battle_controller.list);
+  app.get('/api/all', battle_controller.list);
   app.get('/api/ratings', battle_controller.ratings); 
-
   app.get('/api/:king', battle_controller.fetch); 
 };
